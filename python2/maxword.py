c34ed09673593720fc090be6e3c1746708c611bc
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-05 07:46:55 +0530

    Update text files and refactor maxword.py to find the longest line

--- python2/maxword.py
+++ python2/maxword.py
@@ -1,7 +1,7 @@
-with open ("length.txt", "w+")as file:
-    content=input("Enter your content")
-    file.write(content)
-    file.seek(0)
-    words=file.read().split()
-    longest=max(words, key=len)
-    print(f"Longest word is:", {longest})
+with open("data.txt", "r") as file:
+    lines = file.readlines()  # Read all lines into a list
+
+longest_line = max(lines, key=len)  # Find the longest line using max()
+
+print("Longest Line:", longest_line)
+print("Length of Longest Line:", len(longest_line))
